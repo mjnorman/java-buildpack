e6994746586385c30fd36ad6dd657db297869df8
Michael Norman <michael_norman@cable.comcast.com>
2018-08-31 13:45:18 -0500

    changing path

--- lib/java_buildpack/buildpack.rb
+++ lib/java_buildpack/buildpack.rb
@@ -86,8 +86,9 @@ module JavaBuildpack
       @logger.debug { "Entering release".white.bold }
       value = `echo is this thing working`
       @logger.debug { value.white.bold }
-      value = `ls -al /tmp/app/.java-buildpack/tomcat/webapps`
-      value = `pwd`
+      value = `ls -al /home/vcap/app/.java-buildpack/tomcat/webapps`
+      @logger.debug { value.white.bold }
+      value = `ls -al /home/vcap/app`
       @logger.debug { value.white.bold }
       container = component_detection('container', @containers, true).first
       no_container unless container
